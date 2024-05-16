dd017f3a1b37ddbb676deb564341f97f517a72af
weru87 <weru87@gmail.com>
2024-05-16 20:40:19 +0200

    get human choice

--- rpsgame.js
+++ rpsgame.js
@@ -6,4 +6,17 @@ function getComputerChoice(){
     return gameArray[computerChoice];
 }
 
+function getHumanChoice(){
+    const humanChoice = prompt("Write one of the following: 'rock', 'paper', 'scissors'");
+    if (humanChoice === 'rock' || humanChoice === 'paper' || humanChoice === 'scissors') {
+        return humanChoice;
+    } else {
+        alert("you have to type 'rock', 'paper' or 'scissors'");
+        getHumanChoice();
+    }
+
+}
+
+
 console.log(getComputerChoice());
+getHumanChoice();
